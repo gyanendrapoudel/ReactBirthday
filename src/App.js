a4b512e9abf6a2b39d95debc8979844cc5c7cf98
Gyanendra <gyanen56@gmail.com>
2024-06-10 20:42:30 +1000

    Displaying user with their details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-
+import User from './component/User';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+  const fetchUsers = async ()=>{
+    const response = await fetch("https://randomuser.me/api?results=6")
+    const data = await response.json()
+    const users = data.results;
+    setUsers(users)
+    
+  }
+  useEffect(()=>{
+    fetchUsers()
+  },[])
   return (
-    <div className="App">
-      <h4>hello</h4>
+    <div className="App py-5 ">
+      <div className="container bg-light w-75 m-auto rounded-3 shadow-lg">
+        <h1 className=" py-2">Birthday Reminder</h1>
+        {
+          users.map((user, index)=>{
+            return <User user={user} />
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/component/User.js
@@ -0,0 +1,21 @@
+const User = ({user}) => {
+    console.log(user)
+  return (
+    <div className="ps-5 d-flex mb-3">
+      <img
+        src={user.picture.thumbnail}
+        alt=""
+        style={{ width: '150px' }}
+        className="rounded-circle"
+      />
+      <div className=" ms-3 pt-3">
+        <p className="fw-bolder fs-5 p-0 m-0">
+          Name: {user.name.first} {user.name.last}
+        </p>
+        <p className=" fs-7 p-0 m-0">DOB: {user.dob.date}</p>
+        <p className=" fs-7 p-0 m-0">Age : {user.dob.age}</p>
+      </div>
+    </div>
+  )
+}
+export default User
